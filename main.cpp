1d314c1865353117debc709041578cf0d84e1164
X-MASTER1 <91365977+X-MASTER1@users.noreply.github.com>
2021-09-25 14:32:39 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello and Welcome To Addition Program"<<endl;
+    cout<<"\n";
+    int a;
+    cout<<"Enter Your 1st Number Here:"<<endl;
+    cin>>a;
+    int b;
+    cout<<"Enter Your 2nd Number Here:"<<endl;
+    cin>>b;
+    int c;
+    c=a+b;
+     cout<<"Here Is Your Addition Output:"<<endl;
+    cout<<c;
+    return 0;
+}
